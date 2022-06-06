14be3b735f75149241a13182f9c74b3b11a51ea2
vishnu244 <vishnunali244@gmail.com>
2022-06-06 16:36:50 +0530

    [Vishnu]-Retrieve Employee PayRoll Data in DataBase

--- PayrollService.sql
+++ PayrollService.sql
@@ -2,3 +2,6 @@ create database PayRollService
 use PayRollService
 create table employee_payroll(ID int identity(1,1) primary key,Name varchar(100),Salary int,Start_Date date)
 INSERT into employee_payroll values ('Vishnu',10000,'2022-10-04'),('Shravanthi',20000,'2022-01-04'),('Roshni',30000,'2022-05-04')
+SELECT salary FROM employee_payroll
+WHERE name = 'Vishnu'
+select * from employee_payroll
