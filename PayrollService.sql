d45cc944f4827e3c8465f2982cea3b8328d8a29e
vishnu244 <vishnunali244@gmail.com>
2022-06-06 16:29:51 +0530

    [Vishnu]-Created Employee PayRoll Data in DataBase Using INSERT_INTO(CRUD operator)

--- PayrollService.sql
+++ PayrollService.sql
@@ -1,3 +1,4 @@
 create database PayRollService
 use PayRollService
 create table employee_payroll(ID int identity(1,1) primary key,Name varchar(100),Salary int,Start_Date date)
+INSERT into employee_payroll values ('Vishnu',10000,'2022-10-04'),('Shravanthi',20000,'2022-01-04'),('Roshni',30000,'2022-05-04')
