dbf7a2fd296bac3c2468f20c1fb56b1a86675d7d
vishnu244 <vishnunali244@gmail.com>
2022-06-06 17:25:00 +0530

    [Vishnu]-Added Gender Column and Updated Values to Employee_PayRoll Data in DataBase

--- PayrollService.sql
+++ PayrollService.sql
@@ -2,7 +2,12 @@ create database PayRollService
 use PayRollService
 create table employee_payroll(ID int identity(1,1) primary key,Name varchar(100),Salary int,Start_Date date)
 INSERT into employee_payroll values ('Vishnu',10000,'2022-10-04'),('Shravanthi',20000,'2022-01-04'),('Roshni',30000,'2022-05-04')
+INSERT into employee_payroll values ('Krishna',10000,'2022-03-04','male')
+INSERT into employee_payroll values ('Shiva',10000,'2022-03-04','')
 SELECT salary FROM employee_payroll
 WHERE name = 'Vishnu'
 select * from employee_payroll
 WHERE Start_Date BETWEEN CAST('2022-01-01'AS DATE) AND GETDATE();
+Alter table employee_payroll add Gender varchar(10) 
+update employee_payroll set Gender = 'male' where name = 'vishnu' or name = 'Shiva'
+update employee_payroll set Gender = 'Female' where name = 'shravanthi' 
